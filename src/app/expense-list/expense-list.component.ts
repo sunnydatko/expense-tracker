50b9e7cd6a7eb56106fe6301c13b53d0ed40d911
Sunny <sunnydatko@gmail.com>
2020-02-02 17:09:10 -0800

    added running total to expense list

--- src/app/expense-list/expense-list.component.ts
+++ src/app/expense-list/expense-list.component.ts
@@ -13,7 +13,13 @@ import { ExpenseData } from "../interfaces/expense.interface";
 export class ExpenseListComponent implements OnInit {
   expenses$: Observable<ExpenseData[]>;
   dataSource = new MatTableDataSource();
-  displayedColumns: string[] = ["cost", "quantity", "description", "total"];
+  displayedColumns: string[] = [
+    "cost",
+    "quantity",
+    "description",
+    "total",
+    "running-total"
+  ];
 
   constructor(private store: Store<{ expenses: ExpenseData[] }>) {
     this.expenses$ = store.pipe(select("expenses"));
@@ -21,7 +27,21 @@ export class ExpenseListComponent implements OnInit {
 
   ngOnInit(): void {
     this.expenses$.subscribe(data => {
-      this.dataSource.data = data['expenses'];
+      this.dataSource.data = data["expenses"];
+    });
+  }
+
+  getRunningTotal(index: number): string {
+    let data = <ExpenseData[]>this.dataSource.data;
+    let totals = data.map(a => a.cost * a.quantity);
+    let runningTotal = 0;
+    
+    for (let i = 0; i < index + 1; i++) {
+      runningTotal += totals[i];
+    }
+
+    return runningTotal.toLocaleString(undefined, {
+      minimumFractionDigits: 2
     });
   }
 }
